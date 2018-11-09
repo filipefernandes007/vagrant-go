cfb4aaea6e266362982152f7e6b7d94d42c20698
Filipe Fernandes <filipefernandes007@gmail.com>
2018-11-09 15:02:02 +0000

    Change Node.js to version 10 LTS

--- scripts/setup-nodejs.sh
+++ scripts/setup-nodejs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -q -y nodejs
 sudo apt-get install -q -y build-essential
 
